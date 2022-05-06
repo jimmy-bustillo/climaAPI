2834b9108eff8a543b2254dc7089677bd0df146f
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-05-05 21:40:22 -0600

    Valida el formulario y muestra una alerta si los campos estan vacios

--- js/app.js
+++ js/app.js
@@ -11,6 +11,46 @@ function buscarClima(e) {
   e.preventDefault();
 
   // Validar el formulario
+  const ciudad = document.querySelector("#ciudad").value;
+  const pais = document.querySelector("#pais").value;
+
+  if (ciudad === "" || pais === "") {
+    mostrarAlerta("Ambos campos son obligatorios");
+  }
 
   // Consultar la API
+}
+
+function mostrarAlerta(mensaje) {
+  const alerta = document.querySelector(".bg-red-100");
+
+  if (!alerta) {
+    const alerta = document.createElement("div");
+
+    alerta.classList.add(
+      "bg-red-100",
+      "border-red-400",
+      "text-red-700",
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-md",
+      "mx-auto",
+      "mt-6",
+      "text-center"
+    );
+
+    alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="blog">${mensaje}</span>
+    `;
+
+    container.appendChild(alerta);
+
+    // Se elimina la alerta despues de 5 segundos
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 5000);
+  }
 }
